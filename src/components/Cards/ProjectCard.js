a8f81978275e2af3057fc1b79a5b9793318b5af4
Dawid Urbaniak <udawid00@gmail.com>
2023-12-05 19:04:23 +0100

    Card image

--- src/components/Cards/ProjectCard.js
+++ src/components/Cards/ProjectCard.js
@@ -1,8 +1,40 @@
 import React from "react";
 import { styled } from "styled-components";
 
-const ProjectCard = () => {
-  return <div>Project</div>;
+const Card = styled.div`
+  width: 330px;
+  height: 490px;
+  background-color: black;
+  cursor: pointer;
+  border-radius: 10px;
+  box-shadow: 0 0 12px 4px rgba(0, 0, 0, 0.4);
+  overflow: hidden;
+  transition: all 0.5s ease-in-out;
+  padding: 26px 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+
+  &: hover {
+    transform: translateY(-10px);
+    box-shadow: 0 0 50px 4px rgba(0, 0, 0, 0.6);
+    filter: brightness(1.1);
+  }
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 180px;
+`;
+
+const ProjectCard = ({ project }) => {
+  return (
+    <Card>
+      <Image src={project.image} />
+      <Tags></Tags>
+      <Details></Details>
+    </Card>
+  );
 };
 
 export default ProjectCard;
